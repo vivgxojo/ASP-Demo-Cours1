d0db046da3d68d64a54f5623bdfc6b693d63e195
Maryse M. <Maryse.Mongeau@cegepoutaouais.qc.ca>
2025-05-07 09:22:09 -0400

    Tests unitaires

---
+++ ProjetTestsDemo/TestsControllerEtudiant.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cours1.Controllers;
+using cours1.Models;
+using cours1.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetTestsDemo
+{
+    public class TestsControllerEtudiant
+    {
+        [Fact]
+        public void TestIndex()
+        {
+            EtudiantController etudiantController = new EtudiantController();
+            var result = etudiantController.Index();
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.NotNull(viewResult.Model);
+            var model = Assert.IsType<Classe>(viewResult.Model);
+            Assert.Equal("Maryse", model.Enseignant);
+            Assert.Equal("ASP.Net", model.Cours);
+        }
+        [Fact]
+        public void TestFind()
+        {
+            EtudiantController etudiantController = new EtudiantController();
+            var result = etudiantController.Find(1);
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal(1, viewResult.ViewData["EtudiantId"]);
+            var model = Assert.IsType<Etudiant>(viewResult.Model);
+            Assert.Equal("Alex", model.Nom);
+            Assert.Equal(1, model.NumeroEtudiant);
+        }
+        [Fact]
+        public void TestNotFind()
+        {
+            EtudiantController etudiantController = new EtudiantController();
+            var error = etudiantController.Find(-1);
+            var viewResult1 = Assert.IsType<ViewResult>(error);
+            Assert.Null(viewResult1.Model);
+        }
+
+    }
+}
---
+++ ProjetTestsDemo/TestsControllerHome.cs
@@ -0,0 +1,34 @@
+﻿using cours1.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetTestsDemo
+{
+    public class TestsControllerHome
+    {
+        [Fact]
+        public void TestIndex()
+        {
+            HomeController homeController = new HomeController();
+            var result = homeController.Index();
+            Assert.IsType<ViewResult>(result);
+
+        }
+        [Fact]
+        public void TestHello()
+        {
+            HomeController homeController = new HomeController();
+            var result = homeController.Hello("Philippe");
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("Philippe", viewResult.ViewData["Nom"]);
+            var error = homeController.Hello("");
+            Assert.IsType<ViewResult>(error);
+        }
+        [Fact]
+        public void TestPrivacy()
+        {
+            HomeController homeController = new HomeController();
+            var result = homeController.Privacy();
+            Assert.IsType<ContentResult>(result);
+        }
+    }
+}
